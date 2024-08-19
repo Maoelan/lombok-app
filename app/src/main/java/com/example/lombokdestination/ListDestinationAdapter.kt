58f9156297e43a2427948c21a012c14e58b431e3
Maulana Muhammad <maulanamuhammad2000@gmail.com>
2024-08-19 12:55:16 +0700

    fix ViewHolder

--- app/src/main/java/com/example/lombokdestination/ListDestinationAdapter.kt
+++ app/src/main/java/com/example/lombokdestination/ListDestinationAdapter.kt
@@ -1,23 +1,30 @@
 package com.example.lombokdestination
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class ListDestinationAdapter(private val listDestination: ArrayList<Destination>) : RecyclerView.Adapter<ListDestinationAdapter.ListViewHolder>(){
     class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+        val imgPhoto: ImageView = itemView.findViewById(R.id.destination_item_photo)
+        val tvDestination: TextView = itemView.findViewById(R.id.tv_destination_name)
+        val tvOverview: TextView = itemView.findViewById(R.id.tv_destination_overview)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
-        TODO("Not yet implemented")
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_destination, parent, false)
+        return ListViewHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = listDestination.size
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val (destination, overview, photo) = listDestination[position]
+        holder.imgPhoto.setImageResource(photo)
+        holder.tvDestination.text = destination
+        holder.tvOverview.text = overview
     }
 }
--- app/src/main/java/com/example/lombokdestination/MainActivity.kt
+++ app/src/main/java/com/example/lombokdestination/MainActivity.kt
@@ -5,10 +5,39 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rvDestination: RecyclerView
+    private val list = ArrayList<Destination>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvDestination = findViewById(R.id.rv_destinations)
+        rvDestination.setHasFixedSize(true)
+
+        list.addAll(getListDestinations())
+        showRecyclerList()
+    }
+
+    private fun getListDestinations(): ArrayList<Destination> {
+        val dataDestination = resources.getStringArray(R.array.destination_name)
+        val dataOverview = resources.getStringArray(R.array.destination_overview)
+        val dataPhoto = resources.obtainTypedArray(R.array.destination_photo)
+        val listDestination = ArrayList<Destination>()
+        for (i in dataDestination.indices){
+            val destination = Destination(dataDestination[i], dataOverview[i], dataPhoto.getResourceId(i, -1))
+            listDestination.add(destination)
+        }
+        return listDestination
+    }
+
+    private fun showRecyclerList() {
+        rvDestination.layoutManager = LinearLayoutManager(this)
+        val listDestinationAdapter = ListDestinationAdapter(list)
+        rvDestination.adapter = listDestinationAdapter
     }
 }
